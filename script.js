f704684c4ee8aa78dae38d1e1288900870a135fa
Ryanhangralim <ryanhangralim@gmail.com>
2024-04-17 18:19:14 +0800

    Added pizza size functions

--- script.js
+++ script.js
@@ -4,6 +4,9 @@
 const pizza_1 = document.getElementById("pizza1");
 const pizza_2 = document.getElementById("pizza2");
 const pizza_3 = document.getElementById("pizza3");
+const small_size = document.getElementById("small");
+const medium_size = document.getElementById("medium");
+const large_size = document.getElementById("large");
 const toppings = document.querySelectorAll("input[name='toppings']");
 const price_label = document.getElementById("price");
 let total_price = 0;
@@ -84,7 +87,29 @@ function add_pizza_price(event) {
   }
 }
 
+//function to update pizza size price
+let previous_selected_size = null;
+function add_size_price(event) {
+  //Get current size value and add to total price
+  let selected_size_value = Number(event.target.value);
+  total_price += selected_size_value;
+  if (previous_selected_pizza) {
+    total_price -= previous_selected_size;
+  }
+
+  //hold previous selected size value
+  previous_selected_size = selected_size_value;
+
+  //prevents -1 in total price when user selects small without selecting pizza
+  if (price_label.textContent != "$0") {
+    update_price();
+  }
+}
+
 //Add event listerners to buttons
 pizza_1.addEventListener("change", add_pizza_price);
 pizza_2.addEventListener("change", add_pizza_price);
 pizza_3.addEventListener("change", add_pizza_price);
+small_size.addEventListener("change", add_size_price);
+medium_size.addEventListener("change", add_size_price);
+large_size.addEventListener("change", add_size_price);
